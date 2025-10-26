ccd9c5123bfc3aea92654a0d27069d949b88d87b
Cong Khai <dangcongkhai2k5@gmail.com>
2025-10-26 10:00:21 +0700

    feat: add options for choosing model

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 import torch.nn as nn
 import torch
-from torchvision import transforms
+from torchvision import transforms, models
 from PIL import Image
 from dotenv import load_dotenv
 import os
@@ -11,28 +11,69 @@ from google import genai
 import tempfile
 
 
-LABELS = ["Banh mi", "Mi Quang", "Pho"]
-transform = transforms.Compose(
-    [
-        transforms.ToTensor(),
-        transforms.Resize((224, 224)),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ]
-)
-
-GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
-if GEMINI_API_KEY == "":
-    load_dotenv()
-    GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 ROOT_DIR = os.path.abspath(".")
-MODEL_WEIGHT_PATH = os.path.join(ROOT_DIR, "model_weights.pth")
-INGREDIENTS_EXTRATION_INSTRUCTION = """
-    From the given image, please describe the ingredients that you see and return them in a valid json format as followed:
-    {
-        "ingredients" : ['beef', 'pepper', 'egg']
-    }
-    Remember not to make up any ingredients that you don't find in the image
-"""
+
+
+MODELS = {
+    "SimpleCNN": {
+        "label": ["Banh mi", "Mi Quang", "Pho"],
+        "transform": transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Resize((224, 224)),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        ),
+        "model_weight_path": os.path.join(ROOT_DIR, "model_weights.pth"),
+        "description": "SimpleCNN can only recognize: Banh mi, Mi Quang, Pho",
+    },
+    "MobileNet": {
+        "label": [
+            "Banh beo",
+            "Banh bot loc",
+            "Banh can",
+            "Banh canh",
+            "Banh chung",
+            "Banh cuon",
+            "Banh duc",
+            "Banh gio",
+            "Banh khot",
+            "Banh mi",
+            "Banh pia",
+            "Banh tet",
+            "Banh trang nuong",
+            "Banh xeo",
+            "Bun bo Hue",
+            "Bun dau mam tom",
+            "Bun mam",
+            "Bun rieu",
+            "Bun thit nuong",
+            "Ca kho to",
+            "Canh chua",
+            "Cao lau",
+            "Chao long",
+            "Com tam",
+            "Goi cuon",
+            "Hu tieu",
+            "Mi quang",
+            "Nem chua",
+            "Pho",
+            "Xoi xeo",
+        ],
+        "transform": transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        ),
+        "model_weight_path": os.path.join(ROOT_DIR, "mobilenet_weights.pth"),
+        "description": "MobileNet was trained on 30 Vietnamese foods including Banh mi, Mi Quang, Pho, Banh xeo, etc!",
+    },
+}
 
 
 class FoodCNN(nn.Module):
@@ -56,24 +97,66 @@ class FoodCNN(nn.Module):
             nn.Dropout(0.3),
             nn.Linear(in_features=64, out_features=32),
             nn.ReLU(),
-            nn.Linear(32, len(LABELS)),
+            nn.Linear(32, len(MODELS["SimpleCNN"]["label"])),
         )
 
     def forward(self, X):
         return self.sequential(X)
 
 
+class FoodMobileNet(nn.Module):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.feature_extractor = models.mobilenet_v3_small(
+            weights=models.MobileNet_V3_Small_Weights.IMAGENET1K_V1
+        )
+
+        # Freeze model weights
+        for param in self.feature_extractor.parameters():
+            param.requires_grad = False
+        num_ftrs = self.feature_extractor.classifier[-1].in_features
+        # Replace head
+        self.feature_extractor.classifier = self.feature_extractor.classifier[:-1]
+        self.fc = nn.Linear(num_ftrs, len(MODELS["MobileNet"]["label"]))
+
+    def forward(self, X):
+        X = self.feature_extractor(X)
+        X = self.fc(X)
+        return X
+
+
 # Set up deep learning model
 @st.cache_resource
-def load_model():
-    model = FoodCNN()
-    state_dict = torch.load(MODEL_WEIGHT_PATH, weights_only=True, map_location="cpu")
-    model.load_state_dict(state_dict)
+def load_model(model_name):
+    model_weight_path = MODELS[model_name]["model_weight_path"]
+    model = FoodCNN() if model_name == "SimpleCNN" else FoodMobileNet()
+    try:
+        state_dict = torch.load(
+            model_weight_path, weights_only=True, map_location="cpu"
+        )
+        model.load_state_dict(state_dict)
+    except:
+        pass
     model.eval()
     return model
 
 
-model = load_model()
+INGREDIENTS_EXTRATION_INSTRUCTION = """
+            From the given image, please describe the ingredients that you see and return them in a valid json format as followed:
+            {
+                "ingredients" : ['beef', 'pepper', 'egg']
+            }
+            Remember not to make up any ingredients that you don't find in the image
+        """
+try:
+    GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+    if GEMINI_API_KEY == "":
+        load_dotenv()
+        GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+except:
+    load_dotenv()
+    GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 
 # Set up gemini client
@@ -116,8 +199,20 @@ def get_ingredients_from_food_image(image_file: str):
 # UI
 st.markdown("# Vietnamse food classification and ingredients extraction")
 st.markdown(
-    "This web app can classify 3 dishes such as Pho, Mi Quang and Banh Mi. It can then extract ingredients that the model sees in the image!"
+    "This web app is integrated with 2 different deep learning model that support for Vietnamese food classification with AI backed ingredients extraction"
 )
+
+model_name = st.selectbox(
+    "Choose your model: ",
+    tuple(MODELS.keys()),
+)
+
+model = load_model(model_name)
+transform = MODELS[model_name]["transform"]
+description = MODELS[model_name]["description"]
+labels = MODELS[model_name]["label"]
+
+st.markdown(f"**Model description**: {description}")
 
 
 uploaded_file = st.file_uploader("Upload your image", type=["jpg", "jpeg", "png"])
@@ -137,7 +232,7 @@ if uploaded_file is not None:
             probs = nn.Softmax(dim=1)(outputs)
             top_prob, top_catid = torch.topk(probs, 1)
 
-        pred_label = LABELS[top_catid[0].item()]
+        pred_label = labels[top_catid[0].item()]
         st.success(
             f"Prediction: **{pred_label}** ({top_prob.item()*100:.2f}% confidence)"
         )
